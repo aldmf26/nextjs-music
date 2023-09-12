45d9eed32e494e0566d73598a8b9193f0b2d156b
aldmf26 <aldimf26@gmail.com>
2023-09-12 17:00:16 +0800

    321

---
+++ app/tracks/page.js
@@ -0,0 +1,123 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <Navbar />
+      <main className="grid place-items-center bg-gradient-to-t from-blue-200 to-grey-900 p-5">
+        <div>
+          <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold text-gray-200 mb-5">
+            Explore Tracks
+          </h1>
+          <section className="grid grid-cols-2 sm:grid-cols-5 gap-4">
+            <div className="bg-gray-900 shadow-lg rounded p-2">
+              <div className="group relative">
+                <img
+                  className="w-full md:w-72 block rounded"
+                  src="https://upload.wikimedia.org/wikipedia/en/f/f1/Tycho_-_Epoch.jpg"
+                  alt
+                />
+                <div className="absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
+                  <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={40}
+                      height={40}
+                      fill="currentColor"
+                      className="bi bi-play-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <div className="p-5">
+                <h3 className="text-white text-lg">Epoch</h3>
+                <p className="text-gray-400">Tycho</p>
+              </div>
+            </div>
+            <div className="bg-gray-900 shadow-lg rounded p-2">
+              <div className="group relative">
+                <img
+                  className="w-full md:w-72 block rounded"
+                  src="https://upload.wikimedia.org/wikipedia/en/c/ca/Tycho_-_Awake.png"
+                  alt
+                />
+                <div className="absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
+                  <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={40}
+                      height={40}
+                      fill="currentColor"
+                      className="bi bi-play-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <div className="p-5">
+                <h3 className="text-white text-lg">Awake</h3>
+                <p className="text-gray-400">Tycho</p>
+              </div>
+            </div>
+            <div className="bg-gray-900 shadow-lg rounded p-2">
+              <div className="group relative">
+                <img
+                  className="w-full md:w-72 block rounded"
+                  src="https://upload.wikimedia.org/wikipedia/en/1/11/Dive_tycho_album.jpg"
+                  alt
+                />
+                <div className="absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
+                  <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={40}
+                      height={40}
+                      fill="currentColor"
+                      className="bi bi-play-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <div className="p-5">
+                <h3 className="text-white text-lg">Dive</h3>
+                <p className="text-gray-400">Tycho</p>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+
+      {/* <div className="flex items-center py-10 px-5">
+        <div class="w-1/6 h-60 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <a href="#">
+            <img
+              class="p-8 rounded-t-lg"
+              src="https://images.unsplash.com/photo-1618537993538-0690d0976a48?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFwZXJ8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+              alt="product image"
+            />
+          </a>
+          <div class="flex justify-center pb-5">
+            <a href="#">
+              <span class="bg-blue-100 items-center text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-4">
+                128 BPM
+              </span>
+              <h5 class="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">
+                Ginto
+              </h5>
+            </a>
+          
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+}
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,149 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
   return (
     <div>
-      
+      {/* navbar 1 */}
+      <nav class="flex justify-between px-20 py-3 items-center bg-[#141617]">
+        <h1 class="text-xl text-[#E6E2DC]-800 font-bold">
+          <Link href="/">ALdMF Music</Link>
+        </h1>
+        <div className="flex items-center"></div>
+
+        <div class="flex items-center">
+          <ul class="flex items-center space-x-6">
+            <li>
+              <form>
+                <label
+                  htmlFor="default-search"
+                  className="mb-2 text-sm font-medium text-[#E6E2DC]-900 sr-only dark:text-white"
+                >
+                  Search
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                      className="w-4 h-4 text-[#E6E2DC]-500 dark:text-[#E6E2DC]-400"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                      />
+                    </svg>
+                  </div>
+                  <input
+                    type="search"
+                    id="default-search"
+                    className="block w-full p-2 pl-10 text-sm text-[#E6E2DC]-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="What are you looking for ?"
+                    required
+                  />
+                </div>
+              </form>
+            </li>
+            <li class="font-semibold text-[#E6E2DC]-900">
+              <Link href="/">Home</Link>
+            </li>
+            <li class="font-semibold text-[#E6E2DC]-900">
+              <Link href="/tracks">Tracks</Link>
+            </li>
+            <li>
+              <a target="_blank" href="https://www.instagram.com/aldiiimf">
+                <img
+                  width="48"
+                  height="48"
+                  src="https://img.icons8.com/fluency/48/instagram-new.png"
+                  alt="instagram-new"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://www.youtube.com/channel/UCJk3cbCm938_3gYXA-9pCxw"
+              >
+                <img
+                  width="48"
+                  height="48"
+                  src="https://img.icons8.com/color/48/youtube-play.png"
+                  alt="youtube-play"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://open.spotify.com/artist/1n3Xw0iXi4YEIM2ILuV4vb?si=YMJUqdeSRce5r416ed1pGw"
+              >
+                <img
+                  width="48"
+                  height="48"
+                  src="https://img.icons8.com/color/48/spotify--v1.png"
+                  alt="spotify--v1"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* navbar 2 */}
+      <nav class="bg-cover bg-center bg-[url('../public/img/bgnav.webp')] flex justify-between px-20 py-10 items-center bg-[#D1D5DB]">
+        <div class="flex items-center">
+          <div>
+            <h1 class="mb-8 uppercase text-3xl font-extrabold leading-none tracking-tight text-grey-900 md:text-4xl lg:text-4xl dark:text-grey">
+              Your first hit starts here
+            </h1>
+            <div className="relative">
+              <ul class="flex items-center space-x-3 text-white-500">
+                <li class="font-semibold text-[#E6E2DC]-700">
+                  What’s trending type beat right now:
+                </li>
+                <li class="font-semibold text-[#E6E2DC]-700">
+                  <a
+                    href="#"
+                    class="flex items-center justify-center px-3 h-8 text-xs font-medium bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-[#E6E2DC]-700 dark:bg-gray-800 dark:border-gray-700 dark:text-[#E6E2DC]-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Old School
+                  </a>
+                </li>
+                <li class="font-semibold text-[#E6E2DC]-700">
+                  <a
+                    href="#"
+                    class="flex items-center justify-center px-3 h-8 text-xs font-medium bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-[#E6E2DC]-700 dark:bg-gray-800 dark:border-gray-700 dark:text-[#E6E2DC]-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Arabic
+                  </a>
+                </li>
+                <li class="font-semibold text-[#E6E2DC]-700">
+                  <a
+                    href="#"
+                    class="flex items-center justify-center px-3 h-8 text-xs font-medium bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-[#E6E2DC]-700 dark:bg-gray-800 dark:border-gray-700 dark:text-[#E6E2DC]-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Latin
+                  </a>
+                </li>
+                <li class="font-semibold text-[#E6E2DC]-700">
+                  <a
+                    href="#"
+                    class="flex items-center justify-center px-3 h-8 text-xs font-medium bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-[#E6E2DC]-700 dark:bg-gray-800 dark:border-gray-700 dark:text-[#E6E2DC]-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Instrumental Hip Hop
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
