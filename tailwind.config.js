66dc19f79d0aae75c4853f53d2ec1336c217d8ff
aldmf26 <aldimf26@gmail.com>
2023-09-12 15:34:47 +0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'warna1': '#1D0346',
+        'warna2': '#00337D',
+        'warna3': '#005FA9',
+        'warna4': '#008CC4',
+        'warna5': '#00B8CD',
+        'warna6': '#00E3C6'
+      },
     },
   },
   plugins: [],
