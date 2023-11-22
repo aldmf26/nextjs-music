263b5dc63bf393a94acdc86137f852298857fd08
aldmf26 <aldimf26@gmail.com>
2023-11-22 15:46:51 +0800

    32

--- app/page.js
+++ app/page.js
@@ -51,55 +51,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-      {/* jumbotron */}
-      {/* <nav class="bg-cover object-left-top bg-[url('../public/img/bg.jpg')] flex justify-between px-20 py-44 items-center">
-        <div class="flex items-center text-warnaHeadline">
-          <div>
-            <h1 class="mb-8 uppercase text-3xl font-extrabold leading-none tracking-tight text-grey-900 md:text-4xl lg:text-4xl dark:text-grey">
-              Your first hit starts here
-            </h1>
-            <div className="relative">
-              <ul class="flex items-center space-x-3 text-white-500">
-                <li class="font-semibold text-warnaTeksPrimary-700">
-                  What’s trending type beat right now:
-                </li>
-                <li class="font-semibold text-warnaTeksPrimary-700">
-                  <a
-                    href="#"
-                    class="flex items-center justify-center px-3 h-8 text-xs font-medium bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-warnaTeksPrimary-700 dark:bg-gray-800 dark:border-gray-700 dark:text-warnaTeksPrimary-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    Old School
-                  </a>
-                </li>
-                <li class="font-semibold text-warnaTeksPrimary-700">
-                  <a
-                    href="#"
-                    class="flex items-center justify-center px-3 h-8 text-xs font-medium bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-warnaTeksPrimary-700 dark:bg-gray-800 dark:border-gray-700 dark:text-warnaTeksPrimary-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    Arabic
-                  </a>
-                </li>
-                <li class="font-semibold text-warnaTeksPrimary-700">
-                  <a
-                    href="#"
-                    class="flex items-center justify-center px-3 h-8 text-xs font-medium bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-warnaTeksPrimary-700 dark:bg-gray-800 dark:border-gray-700 dark:text-warnaTeksPrimary-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    Latin
-                  </a>
-                </li>
-                <li class="font-semibold text-warnaTeksPrimary-700">
-                  <a
-                    href="#"
-                    class="flex items-center justify-center px-3 h-8 text-xs font-medium bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-warnaTeksPrimary-700 dark:bg-gray-800 dark:border-gray-700 dark:text-warnaTeksPrimary-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    Instrumental Hip Hop
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav> */}
     </main>
   );
 }
